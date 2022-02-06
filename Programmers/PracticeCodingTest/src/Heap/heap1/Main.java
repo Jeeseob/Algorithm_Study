059647cd17b7caad7411320946b4e1d1ed5c2988
Jeeseob <jeeseob5761@gmail.com>
2022-02-07 00:27:56 +0900

    더 맵게 문제

--- Programmers/PracticeCodingTest/src/Heap/heap1/Main.java
+++ Programmers/PracticeCodingTest/src/Heap/heap1/Main.java
@@ -1,9 +1,9 @@
-package Hash.hash1;
+package Heap.heap1;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Solution solution = new Solution();
-        System.out.println(solution.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        System.out.println(solution.solution( new int[]{1, 2, 3, 9, 10, 12}, 7));
     }
 }
--- Programmers/PracticeCodingTest/src/Heap/heap2/Main.java
+++
@@ -1,9 +0,0 @@
-package Heap.heap1;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    Solution solution = new Solution();
-        System.out.println(solution.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
-    }
-}
