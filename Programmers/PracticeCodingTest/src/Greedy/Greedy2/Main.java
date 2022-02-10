95b4445380e5342c31d6b9664fefed9a70b3eeaa
Jeeseob <jeeseob5761@gmail.com>
2022-02-10 15:28:24 +0900

    조이스틱 문제

---
+++ Programmers/PracticeCodingTest/src/Greedy/Greedy2/Main.java
@@ -0,0 +1,9 @@
+package Greedy.Greedy2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+        System.out.println(solution.solution("JEROEN"));
+    }
+}
