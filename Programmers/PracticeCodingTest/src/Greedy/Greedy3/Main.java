c1f2525035d3801df741b2672729890531278f1c
Jeeseob <jeeseob5761@gmail.com>
2022-02-11 13:27:03 +0900

    큰수 만들기 문제

---
+++ Programmers/PracticeCodingTest/src/Greedy/Greedy3/Main.java
@@ -0,0 +1,11 @@
+package Greedy.Greedy3;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+        System.out.println(solution.solution("1924", 2));
+        System.out.println(solution.solution("1231234", 3));
+        System.out.println(solution.solution("020350123050235021035004213040", 3));
+    }
+}
