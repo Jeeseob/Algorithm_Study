6a7dd584534c0d89dd051f163d3e800510c621fc
Jeeseob <jeeseob5761@gmail.com>
2022-02-22 20:48:59 +0900

    여행 경로 문제

---
+++ Programmers/PracticeCodingTest/src/DFSBFS/DFSBFS4/Main.java
@@ -0,0 +1,9 @@
+package DFSBFS.DFSBFS4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Solution solution = new Solution();
+        System.out.println(solution.solution(new String[][] {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}}));
+    }
+}
