6d6f471c39f44b17faae3dc9c84bd552e8d005fa
Jeeseob <jeeseob5761@gmail.com>
2022-03-06 21:50:38 +0900

    백준 Fly me to the Alpha Centauri 문제

--- BaekJoon/java/src/gold/problem1011/Main.java
+++ BaekJoon/java/src/gold/problem1011/Main.java
@@ -3,45 +3,34 @@ package gold.problem1011;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int answer ;
         Scanner scanner = new Scanner(System.in);
         int repititionNumber = scanner.nextInt();
 
+
         for(int i = 0; i < repititionNumber; i++) {
-            int answer = 0;
             int distanceTraveled = 0;
-            int speed = 1;
+            int count = 0;
 
             int distance = ( scanner.nextInt() * (-1) ) + scanner.nextInt();
 
-            System.out.println("distance = " + distance);
+            // 횟수별로, 가능한 최고 값이 정해져 있다.
+            while (distance/2 + distance%2 > distanceTraveled) {
+                count++;
+                distanceTraveled += count;
+            }
 
-            while (distance/2 > distanceTraveled) {
-                distanceTraveled += speed;
-                speed++;
-                answer++;
-            }
-            System.out.println("distanceTraveled = " + distanceTraveled);
-            System.out.println("answer = " + answer);
-            System.out.println("speed = " + speed);
+            answer = (2 * count) - 1;
 
-            if(distance%2 == 0) {
-                if(distance/2 < distanceTraveled) {
-                    answer = answer * 2 - 1;
-                }
-                else {
-                    answer = answer * 2;
-                }
+            // if(distanceTraveled * 2 - count < distance)
+            // 위의 방법으로 하면, distance가 2^31 즉 int로 표현 가능한 최대 값일 때, 오버플로우가 나서, 정확한 비교가 안된다...
+
+            if(distanceTraveled < (distance + count) / 2 + (distance + count) % 2) {
+                answer += 1;
             }
-            else {
-                if(distance/2 == distanceTraveled) {
-                    answer = answer * 2 + 1;
-                }
-                else {
-                    answer = answer * 2;
-                }
-            }
-                System.out.println(answer);
+            System.out.println(answer);
         }
     }
 }
+
---
+++ BaekJoon/java/src/gold/problem9663/Main.java
@@ -0,0 +1,14 @@
+package gold.problem9663;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int board = scanner.nextInt();
+
+
+    }
+}
