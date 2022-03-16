2d50af8282a0ea1750d4d20f9e1321cbae242ed0
Jeeseob <jeeseob5761@gmail.com>
2022-03-16 16:28:15 +0900

    velog 업로드 및 수정

--- BaekJoon/java/src/gold/problem1043/Main.java
+++ BaekJoon/java/src/gold/problem1043/Main.java
@@ -15,14 +15,15 @@ public class Main {
 
         strings = scanner.nextLine().split(" ");
 
-        ArrayList<Integer> knownRealPeople = new ArrayList<Integer>();
+        ArrayList<Integer> knowReals = new ArrayList<Integer>();
         for(String str : strings) {
-            knownRealPeople.add(Integer.parseInt(str));
+            knowReals.add(Integer.parseInt(str));
         }
-        knownRealPeople.remove(0); // 처음 값은 총 인원이기 때문에 제외
+        knowReals.remove(0); // 처음 값은 총 인원이기 때문에 제외
 
         ArrayList<ArrayList<Integer>> peopleInParty = new ArrayList<ArrayList<Integer>>();
 
+        //파티와 각 파티에 참석하는 사람을 저장한다.
         for(int i = 0; i<numberOfParty; i++) {
             peopleInParty.add(new ArrayList<Integer>());
             strings = scanner.nextLine().split(" ");
@@ -32,13 +33,17 @@ public class Main {
             peopleInParty.get(i).remove(0);
         }
 
-        // 광기의 knownRealPeople... 찾기
+        // 광기의 knownReals.. 찾기
+
+        // 모든 파티를 체크하는데, 시간상관없이 체크해야하기 때문엔 이중 for문을 돈다.
         for(int j = 0; j<numberOfParty; j++) {
             for (int i = 0; i < numberOfParty; i++) {
-                for (int person : knownRealPeople) {
-                    if (peopleInParty.get(i).contains(person)) {
+                // knowReals에 속하는 사람이 해당 파티에 존재하는 경우,
+                for (int knowReal : knowReals) {
+                    // 해당 파티에 참석한 모든 사람을 knowReals에 추가한다.
+                    if (peopleInParty.get(i).contains(knowReal)) {
                         for (int personInParty : peopleInParty.get(i)) {
-                            knownRealPeople.add(personInParty);
+                            knowReals.add(personInParty);
                         }
                         break;
                     }
@@ -46,37 +51,17 @@ public class Main {
             }
         }
 
+        // 진실을 아는 사람이 아무도 없는 경우, 해당 파티에서는 거짓말을 해도 된다.
         for(int i = 0; i<numberOfParty; i++) {
-            for(int person : knownRealPeople) {
+            for(int person : knowReals) {
                 if (peopleInParty.get(i).contains(person)) {
                     addPoint = 0;
                     break;
                 }
-                else {
-                    addPoint = 1;
-                }
+                addPoint = 1;
             }
             result += addPoint;
         }
-
-//        for(int person : knownRealPeople) {
-//            System.out.println(person);
-//        }
-//        System.out.println();
         System.out.println(result);
-
-
-
-
-        // 1 진실을 아는 사람이 오면 더하지 않는다.
-        // 2 진실을 아는 사람과 함께 온 사람이 있으면 더하지 않는다.
-
-        // 3 순서대로하는 것이 아니라 한번에 처리해야 한다.
-
-        // 4 일단 진실을 아는 사람과 같이온 사람부터 처리한 후, 그 사람과 같이 온 사람을 진실을 아는 사람에 넣는다.
-
-
-
-
     }
 }
