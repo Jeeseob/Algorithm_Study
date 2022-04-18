e7b54e2a1bade0f626a61aae47cc9595c156a31c
Jeeseob <jeeseob5761@gmail.com>
2022-04-18 19:57:59 +0900

    백준 피보나치 함수 문제

--- BaekJoon/java/src/sliver/problem1003/Main.java
+++ BaekJoon/java/src/sliver/problem1003/Main.java
@@ -3,15 +3,39 @@ package sliver.problem1003;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
+    private static int[] numbers;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if(temp == 0) {
+                System.out.println("1 0");
+                continue;
+            }
+            else if(temp == 1) {
+                System.out.println("0 1");
+                continue;
+            }
+
+            numbers = new int[temp+1];
+            numbers[temp] = 1;
+            calcFibonacci(temp);
+            System.out.println(numbers[0] + " " + numbers[1]);
+        }
+        return;
+    }
+
+    private static void calcFibonacci(int temp) {
+        for(int i = temp; i > 1; i--) {
+            numbers[i - 1] += numbers[i];
+            numbers[i - 2] += numbers[i];
+        }
+        return;
     }
 }
