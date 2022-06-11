608574619476c4f653a9e6050ba3986e0393b852
Jeeseob <jeeseob5761@gmail.com>
2022-06-11 09:34:12 +0900

    백준 리모컨 문제

---
+++ BaekJoon/java/src/gold/problem1107/Main.java
@@ -0,0 +1,62 @@
+package gold.problem1107;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int MAX = 1_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int numberOfBrokenButton = Integer.parseInt(br.readLine());
+
+        boolean[] brokenButtons = initButtons();
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < numberOfBrokenButton; i++) {
+            int tempButton = Integer.parseInt(st.nextToken());
+            brokenButtons[tempButton] = true;
+        }
+
+        // +/-로만 이동했을 때
+        int answer = Math.abs(N - 100);
+
+        // 모든 경우의 수를 시도해야 한다. / brute force
+        for (int temp = 0; temp < MAX; temp++) {
+            int lengthOfTemp = calcLength(temp, brokenButtons);
+            if (lengthOfTemp > 0) {
+                int press = Math.abs(temp - N);
+                answer = Math.min(answer, lengthOfTemp + press);
+            }
+        }
+        System.out.println(answer);
+    }
+
+    static int calcLength(int temp, boolean[] brokenButtons) {
+        if (temp == 0) {
+            if (brokenButtons[0]) {
+                return 0;
+            }
+            return 1;
+        }
+
+        int length = 0;
+        while (temp > 0) {
+            if (brokenButtons[temp % 10]) {
+                return 0;
+            }
+            length++;
+            temp /= 10;
+        }
+        return length;
+    }
+
+    private static boolean[] initButtons() {
+        boolean[] brokenButtons = new boolean[10];
+        Arrays.fill(brokenButtons, false);
+        return brokenButtons;
+    }
+}
