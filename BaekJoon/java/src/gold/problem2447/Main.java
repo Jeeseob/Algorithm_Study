73a0f483176779a840b8c3b84cbb16febd02118c
Jeeseob <jeeseob5761@gmail.com>
2022-08-17 20:12:53 +0900

    백준 별 찍기 - 10 문제

--- BaekJoon/java/src/gold/problem2447/Main.java
+++ BaekJoon/java/src/gold/problem2447/Main.java
@@ -1,5 +1,50 @@
-package gold.problem2447;/**
+package gold.problem2447;
+
+import java.io.*;
+import java.util.Arrays;
+
+/**
  * @Author : Jeeseob
  * @CreateAt : 2022/08/17
- */public class Main {
+ * @Problem : https://www.acmicpc.net/problem/2447
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        char[][] board;
+
+        board = new char[N][N];
+        for (char[] string : board) {
+            Arrays.fill(string, ' ');
+        }
+        star(board, 0, 0, N);
+
+        for (char[] string : board) {
+            for (char data : string) {
+                stringBuilder.append(data);
+            }
+            stringBuilder.append("\n");
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    public static void star(char[][] arr, int xPos, int yPos, int N) {
+        if (N == 1) {
+            arr[xPos][yPos] = '*';
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!(i == 1 && j == 1)) {
+                    star(arr, xPos + i * (N / 3), yPos + j * (N / 3), N / 3);
+                }
+            }
+        }
+    }
 }
