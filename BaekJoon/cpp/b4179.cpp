b02a21721dc24c1899d6fee0ac7c8b7b864b72e2
jeeseob <>
2023-08-23 10:44:45 +0900

    [4179] 불!

--- BaekJoon/cpp/b4179.cpp
+++ BaekJoon/cpp/b4179.cpp
@@ -1,87 +1,97 @@
-#include <bits/stdc++.h>
+/**
+ * https://www.acmicpc.net/problem/4179
+ */
+
+
+#include<iostream>
+#include<queue>
 
 using namespace std;
 
 class Point {
-public:
-    int row;
-    int column;
-
-    Point(int row, int column) {
-        this->row = row;
-        this->column = column;
-    }
+    public:
+        int x;
+        int y;
+        int count;
 };
 
-int main() {
-    int sizeOfRow;
-    int sizeOfColumn;
-    int dx[4] = {1, -1, 0, 0};
-    int dy[4] = {0, 0, 1, -1};
-    queue<Point> fireQueue;
-    queue<Point> jihoonQueue;
 
+int board[1001][1001];
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin >> sizeOfRow >> sizeOfColumn;
-    string board[sizeOfRow + 2];
 
-    int fireDepth[sizeOfRow][sizeOfColumn];
-    int jihoonDepth[sizeOfRow][sizeOfColumn];
-    for(int i = 0; i < sizeOfRow; i++){
-        fill(fireDepth[i], fireDepth[i]+sizeOfColumn, -1);
-        fill(jihoonDepth[i], jihoonDepth[i]+sizeOfColumn, -1);
-    }
+    int maxX;
+    int maxY;
+    int jeeX;
+    int jeeY;
 
-    for (int i = 0; i < sizeOfRow; i++) {
-        cin >> board[i];
-    }
-    for(int i = 0; i < sizeOfRow; i++){
-        for(int j = 0; j < sizeOfColumn; j++){
-            if(board[i][j] == 'F'){
-                fireQueue.push(Point(i, j));
-                fireDepth[i][j] = 0;
-            }
-            if(board[i][j] == 'J'){
-                jihoonQueue.push(Point(i, j));
-                jihoonDepth[i][j] = 0;
-            }
-        }
-    }
+    cin >> maxX >> maxY;
+    queue<Point> pointQueue;
 
+    for(int i = 0; i < maxX; i++) {
+        for(int j = 0; j < maxY; j++) {
+            char temp;
+            cin >> temp;
 
-    while (!fireQueue.empty()) {
-        Point point = fireQueue.front();
-        fireQueue.pop();
-        int nextDepth = fireDepth[point.row][point.column] + 1;
-        for (int i = 0; i < 4; i++) {
-            int nextRow = point.row + dx[i];
-            int nextColumn = point.column + dy[i];
-
-            if (nextRow < 0 || sizeOfRow <= nextRow || nextColumn < 0 || sizeOfColumn <= nextColumn) continue;
-            if (board[nextRow][nextColumn] == '#' || -1 != fireDepth[nextRow][nextColumn]) continue;
-            fireDepth[nextRow][nextColumn] = nextDepth;
-            fireQueue.push(Point(nextRow, nextColumn));
-
-        }
-    }
-
-    while (!jihoonQueue.empty()) {
-        Point point = jihoonQueue.front();
-        jihoonQueue.pop();
-        for (int i = 0; i < 4; i++) {
-            int nextRow = point.row + dx[i];
-            int nextColumn = point.column + dy[i];
-            if (nextRow < 0 || sizeOfRow <= nextRow || nextColumn < 0 || sizeOfColumn <= nextColumn) {
-                cout << jihoonDepth[point.row][point.column] + 1 << "\n";
+            if(temp == '#') {
+                board[i][j] = -2;
+            } else if(temp == '.') {
+                board[i][j] = 0;
+            } else if(temp == 'F') {
+                board[i][j] = -1;
+                pointQueue.push({i,j,-1});
+            } else if(temp == 'J') {
+                jeeX = i;
+                jeeY = j;
+                board[i][j] = 1;
+            } else {
+                cout << "ERROR";
                 return 0;
             }
-            if (board[nextRow][nextColumn] == '#') continue;
-            if (jihoonDepth[nextRow][nextColumn] != -1 || fireDepth[nextRow][nextColumn] <= jihoonDepth[point.row][point.column] + 1) continue;
-            jihoonDepth[nextRow][nextColumn] = jihoonDepth[point.row][point.column] + 1;
-            jihoonQueue.push(Point(nextRow, nextColumn));
         }
     }
-    cout << "IMPOSSIBLE\n";
+
+    // jeehoon은 마지막에 추가
+    pointQueue.push({jeeX,jeeY,1});
+
+    if(jeeX == 0 || jeeX == maxX - 1 || jeeY== 0 || jeeY == maxY -1) {
+        cout << 1;
+        return 0;
+    }
+
+    while(!pointQueue.empty()) {
+        Point tempPoint = pointQueue.front(); pointQueue.pop();
+        for(int i = 0; i < 4; i++ ) {
+            int nextX = tempPoint.x + dx[i];
+            int nextY = tempPoint.y + dy[i];
+            
+            if(nextX < 0 || nextX >= maxX || nextY < 0 || nextY >= maxY) {continue;}
+            if(board[nextX][nextY] < 0) {continue;}
+
+            // jeehoon
+            if(tempPoint.count > 0 && board[nextX][nextY] == 0) {
+                if(nextX == 0 || nextX == maxX - 1 || nextY== 0 || nextY == maxY -1) {
+                    cout << tempPoint.count + 1;
+                    return 0;
+                }
+
+                board[nextX][nextY] = -1;
+                pointQueue.push({nextX, nextY, tempPoint.count + 1});
+            } else if(board[tempPoint.x][tempPoint.y] == -1) {
+                board[nextX][nextY] = -1;
+                pointQueue.push({nextX, nextY, -1});
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE";
     return 0;
 }
+
+// 2 2
+// JF
+// FF
